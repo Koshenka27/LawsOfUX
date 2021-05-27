5e8e5c1db4d8cdc31c183a30a9f53fb204c88a40
Logan Koshenka <koshenka27@gmail.com>
2021-05-27 11:40:08 -0400

    Initial Commit

---
+++ LawsOfUX/LawsOfUXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawsOfUXApp.swift
+//  LawsOfUX
+//
+//  Created by Logan Koshenka on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LawsOfUXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
